105a3b3dff0eb028e8fc18cde3045eb8867b3a4d
royhan31 <izzapark34@gmail.com>
2020-02-04 23:15:45 +0700

    crud search

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -32,7 +32,8 @@ class CategoryController extends Controller
   }
 
   public function search($keyword){
-    $categories = Category::where('name','LIKE','%'.$keyword.'%')->get();
+    $categories = Category::where('name','ILIKE','%'.$keyword.'%')->get(); //postgre
+    // $categories = Category::where('name','ILIKE','%'.$keyword.'%')->get(); //mysql
     return response()->json([
       'message' => 'success',
       'status' => true,
